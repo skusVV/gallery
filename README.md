89ad23f2c57caeb1abcc97adce71f72286166d3b
vskus <vskus@beyond.ai>
2024-06-17 12:15:52 +0300

    updated DB

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## TODO
 
-- [ ] Make it deploy (vercel)
-- [ ] Scaffold basic ui with mock data
-- [ ] Tidy up build process
-- [ ] Actually set up a database (vercel postgres)
-- [ ] Attach database to UI
+- [x] Make it deploy (vercel)
+- [x] Scaffold basic ui with mock data
+- [x] Tidy up build process
+- [x] Actually set up a database (vercel postgres)
+- [x] Attach database to UI
 - [ ] Add authentication (w/ clerk)
 - [ ] Add image upload
 - [ ] "taint" (server-only)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,18 @@
 import Image from 'next/image';
-
 import { db } from '~/server/db/index';
 
 export const dynamic = 'force-dynamic';
 
-const mockUrls = [
-    'https://i.ytimg.com/vi/l4Iu3YB0pTs/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBY3rFFNiVZ7OE7u5--K-AmnenTKw',
-    'https://i.ytimg.com/vi/TKi1de9UuB8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCNmixOeP3161PgMgJggvhE5mju3g',
-    'https://i.ytimg.com/vi/bRIhKox8VNQ/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCXKFzJzXLAUWleMV3BG6gtGFFREg',
-    'https://i.ytimg.com/vi/NZulDyerzrA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDTyQgmmErw_0UoDI6nO8hmF-Q2rA'
-]
-
-const mockImages = mockUrls.map((url, i) => ({id: i + 1, url}))
-
 export default async function HomePage() {
-    const posts = await db.query.posts.findMany();
-    console.log('posts', posts);
+    const images = await db.query.images.findMany();
     return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-          {mockImages.map((image, i) => (
+          {images.map((image) => (
               <div key={image.id} className="p-4">
-                  <Image src={image.url} width={300} height={250} alt="1"/>
+                  <Image src={image.url} width={300} height={250} alt={image.name} />
               </div>
           ))}
-          {
-              posts.map((post, i) => (<div key={post.id}>{post.name}</div>))
-          }
       </div>
     </main>
   );
