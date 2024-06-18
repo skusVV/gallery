eff47df1a10e331439214bf29c93c7f85d4c373d
vskus <vskus@beyond.ai>
2024-06-18 16:50:59 +0300

    before sentry

--- src/app/_components/ImageList.tsx
+++ src/app/_components/ImageList.tsx
@@ -7,10 +7,10 @@ export default async function ImageList() {
     const images = await getMyImages();
 
     return (
-        <div className="flex flex-wrap gap-4">
+        <div className="flex flex-wrap gap-4 justify-center">
             {images.map((image) => (
                 <div key={image.id} className="p-4">
-                    <Image src={image.url} width={300} height={250} alt={image.name}/>
+                    <Image src={image.url} width={200} height={200} alt={image.name}/>
                 </div>
             ))}
         </div>
