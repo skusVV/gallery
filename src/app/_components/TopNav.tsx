08273ce8964ee6f49540b459555061d893c994ac
vskus <vskus@beyond.ai>
2024-06-18 16:08:50 +0300

    refresh after upload

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,7 +1,12 @@
+'use client';
+
 import { SignInButton, SignedOut, SignedIn, UserButton} from '@clerk/nextjs'
 import {UploadButton} from "~/app/utils/uploadthings";
+import {useRouter} from "next/navigation";
 
 export default function TopNav() {
+    const router = useRouter();
+
     return (
         <nav className="flex justify-between p-4 w-full border-b font-semibold">
             <div>Gallery</div>
@@ -11,10 +16,14 @@ export default function TopNav() {
                     <SignInButton />
                 </SignedOut>
                 <SignedIn>
-                    <UploadButton className="mr-16" endpoint="imageUploader"/>
+                    <UploadButton
+                        className="mr-16"
+                        endpoint="imageUploader"
+                        onClientUploadComplete={() => {
+                            router.refresh();
+                        }}/>
                     <UserButton />
                 </SignedIn>
-
             </div>
         </nav>
     )
